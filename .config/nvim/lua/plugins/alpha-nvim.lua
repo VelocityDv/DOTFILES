803d7ead9e4bfb4bfe406f0cf787135a9b69749c
EdenZhangGithub <edenzhangakl@gmail.com>
2022-01-26 20:35:12 +1300

    remove icons from alpha-nvim

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -25,11 +25,11 @@ dashboard.section.header.val = {
 
 -- menu
 dashboard.section.buttons.val = {
-  dashboard.button('e', ' New file', ':ene <BAR> startinsert<CR>'),
-  dashboard.button('f', ' Find file', ':NvimTreeOpen<CR>'),
-  dashboard.button('s', ' Settings', ':e $MYVIMRC<CR>'),
-  dashboard.button('u', ' Update plugins', ':PackerUpdate<CR>'),
-  dashboard.button('q', ' Quit', ':qa<CR>'),
+  dashboard.button('e', 'New file', ':ene <BAR> startinsert<CR>'),
+  dashboard.button('f', 'Find file', ':NvimTreeOpen<CR>'),
+  dashboard.button('s', 'Settings', ':e $MYVIMRC<CR>'),
+  dashboard.button('u', 'Update plugins', ':PackerUpdate<CR>'),
+  dashboard.button('q', 'Quit', ':qa<CR>'),
 }
 
 dashboard.section.footer.val = footer()
